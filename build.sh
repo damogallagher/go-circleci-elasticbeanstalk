e1f893a4732bd95c084aac767a023943dcb4ad40
Damien Gallagher <damien.gallagher@gmail.com>
2020-03-15 17:22:31 +0000

    udpate to files for build

--- build.sh
+++ build.sh
@@ -7,4 +7,8 @@ go get ./...
 # go build ./ -o bin/application
 
 # build command
-go build application.go -o bin/application 
+#go build application.go -o bin/application 
+go build application.go
+
+mkdir bin
+mv application bin/
