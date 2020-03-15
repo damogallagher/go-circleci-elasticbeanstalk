a13fcf7527295f3e67b2f50c5ebbc924c9562983
Damien Gallagher <damien.gallagher@gmail.com>
2020-03-15 14:40:59 +0000

    Initial commit

--- server.go
+++ server.go
@@ -4,22 +4,49 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func main() {
-	// Echo instance
 	e := echo.New()
-
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Route => handler
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	type User struct {
+		Name  string `json:"name" xml:"name" form:"name" query:"name"`
+		Email string `json:"email" xml:"email" form:"email" query:"email"`
+	}
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
 	})
 
-	// Start server
+	//e.POST("/save", save)
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+	//e.PUT("/users/:id", updateUser)
+	//e.DELETE("/users/:id", deleteUser)
+
+	e.Static("/static", "static")
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+//e.GET("/show", show)
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
 }
